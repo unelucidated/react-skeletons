48fd55ed6ab3f058041130a2d0deecf913e3abdc
unelucidated <douglastan95@gmail.com>
2020-10-24 01:11:52 +0800

    fri

---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div className="articles">
+            <h2>User Details</h2>
+        </div>
+    );
+}
+
+export default User;
