0169049e2819320c268f20bccf7b99e640cb1540
unelucidated <douglastan95@gmail.com>
2020-10-24 22:31:32 +0800

    completed skeletons in react

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SkeletonArticle from '../skeletons/SkeletonArticle';
 
 const Articles = () => {
+    const [articles, setArticles] = useState(null);
+    useEffect(() => {
+        setTimeout( async () => {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await res.json();
+            setArticles(data);       
+        }, 100000)
+    })
+
     return (
         <div className="articles">
             <h2>Articles</h2>
+            {articles&&articles.map(article => {
+                return (
+                <div className="articles" key={article.id}>
+                    <h3>{article.title}</h3>
+                    <p>{article.body}</p>
+                </div>
+            )})}
+
+            {!articles&& [1,2,3,4,5].map(n=>{
+                return <SkeletonArticle key={n} theme="dark"/>
+            })}
         </div>
     );
 }
